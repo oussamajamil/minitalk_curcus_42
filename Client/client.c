309223cdf42944b6db65666d63f7b42bd7ddff4f
Oussama Jamil <ojamil@e2r2p9.1337.ma>
2021-12-22 19:07:32 +0100

    2 push

--- Client/client.c
+++ Client/client.c
@@ -6,21 +6,21 @@
 /*   By: ojamil <ojamil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 11:25:10 by ojamil            #+#    #+#             */
-/*   Updated: 2021/12/22 15:35:57 by ojamil           ###   ########.fr       */
+/*   Updated: 2021/12/22 19:00:25 by ojamil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<unistd.h>
+#include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
 #include "./../minitalk.h"
 
-int ft_Controller_pid(int argc,char *argv[])
+int ft_Controller_pid(int argc, char *argv[])
 {
-	if(argc == 3)
+	if (argc == 3)
 	{
-		int i ;
-		
+		int i;
+
 		i = -1;
 		while (argv[1][++i])
 		{
@@ -29,42 +29,79 @@ int ft_Controller_pid(int argc,char *argv[])
 		}
 		return (0);
 	}
-	return(-1);
+	return (-1);
 }
 
-void ft_send_caractere(int pid,unsigned char c)
+void ft_send_caractere(int pid, unsigned char c)
 {
 	int i;
 
 	i = -1;
 	while (++i < 8)
 	{
-		if(c & 1)
-			kill(pid,SIGUSR2);
+		if (c & 1)
+			kill(pid, SIGUSR2);
 		else
-			kill(pid,SIGUSR1);
+			kill(pid, SIGUSR1);
 		c = c >> 1;
 		usleep(50);
 	}
-	
+}
+void ft_send_message(int pid)
+{
+	int i;
+
+	i = -1;
+	while (++i < 8)
+	{
+		kill(pid, SIGUSR1);
+		usleep(50);
+	}
+}
+// void get_message(int signal)
+// {
+// 	if (signal == SIGUSR1)
+// 	{
+// 		write(1,"hello",5);
+// 	}
+// 	else
+// 		eit(0);
+// }
+void ft_accepte_message(int signal, siginfo_t *info, void *uc)
+{
+	(void)*uc;
+	(void)*info;
+	if (signal == SIGUSR1)
+	{
+		ft_putstr_fd("hello\n", 1);
+	}
 }
 
-int main (int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	int pid;
 	char *text;
 	int i;
-	
-	if (ft_Controller_pid(argc,argv) == 0)
+	struct sigaction action;
+
+	if (ft_Controller_pid(argc, argv) == 0)
 	{
 		i = -1;
 		pid = ft_atoi(argv[1]);
 		text = argv[2];
+		action.sa_sigaction = ft_accepte_message;
+		action.sa_flags = 0;
+		sigemptyset(&action.sa_mask);
 		while (text[++i])
 		{
-			ft_send_caractere(pid,text[i]);
+			ft_send_caractere(pid, text[i]);
 			usleep(50);
 		}
+		sigemptyset(&action.sa_mask);
+		sigaction(SIGUSR1, &action, NULL);
+		sigaction(SIGUSR2, &action, NULL);
+		while (1)
+			usleep(5000);
 	}
 	return (0);
 }
--- Server/server.c
+++ Server/server.c
@@ -6,12 +6,13 @@
 /*   By: ojamil <ojamil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 12:59:30 by ojamil            #+#    #+#             */
-/*   Updated: 2021/12/22 15:18:28 by ojamil           ###   ########.fr       */
+/*   Updated: 2021/12/22 19:03:11 by ojamil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../minitalk.h"
 #include <stdio.h>
+#include <signal.h>
 
 int ft_power(int a,int p)
 {
@@ -22,32 +23,53 @@ int ft_power(int a,int p)
     return(a * ft_power(a,p-1));
 }
 
-void ft_accepte_signale(int signal)
+void ft_accepte_signale(int signal,siginfo_t *info,void *uc)
 {
+	(void) *uc;
+	(void) *info;
 	static int cp = 0 ;
+	static int cp_signal1 = 0;
 	static char caractere;
 
 	if (signal == SIGUSR2)
 	{
 		caractere += ft_power(2,cp);
 		cp++;
+		cp_signal1 = 0;
 	}
 	if (signal == SIGUSR1)
+	{
 		cp++;
-	if(cp == 8)
+		cp_signal1++;
+	}
+	if(cp == 8 && cp_signal1 < 8)
 	{
 		ft_putchar_fd(caractere, 1);
 		cp = 0;
 		caractere = 0;
+		cp_signal1 = 0;
+	}
+	else if (cp_signal1 == 8)
+	{
+		kill(info->si_pid,SIGUSR1);
+		ft_putstr_fd("hello\n",6);
 	}
 }
 int main ()
 { 
+	struct sigaction action;
+	
+	int i;
+
+	i = 0;
 	ft_putstr_fd("Server PID:",1);
 	ft_putnbr_fd(getpid(),1);
 	ft_putchar_fd('\n',1);
-	signal(SIGUSR1,ft_accepte_signale);
-	signal(SIGUSR2,ft_accepte_signale);
+	action.sa_sigaction = ft_accepte_signale;
+	action.sa_flags = 0;
+	sigemptyset(&action.sa_mask);
+	sigaction(SIGUSR1,&action,NULL);
+	sigaction(SIGUSR2,&action,NULL);
 	while (1)
 		usleep(5000);
 	return (0);
